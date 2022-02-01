aaf5b85cf0f97be52499636045e31842e6eda018
Per Olofsson <magervalp@fastmail.fm>
2022-02-01 13:00:00 +0100

    Change current user lookup to use scutil instead of python

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -10,7 +10,8 @@ if [ "$1" = 'manualcheck' ]; then
 fi
 
 # Check if homebrew is installed
-CURRENTUSER=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+CURRENTUSER=$(echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+
 
 if [[ $CURRENTUSER != "" ]]; then
     brew=$(sudo -i -u $CURRENTUSER command -v brew)
