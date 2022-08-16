7b39bd9f517cc95c41083f773e8e4bdcb279c04f
tuxudo <tuxudo@users.noreply.github.com>
2022-08-16 14:54:56 -0400

    Fix for Apple Silicon installs

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -12,15 +12,24 @@ fi
 # Check if homebrew is installed
 CURRENTUSER=$(echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }')
 
-
 if [[ $CURRENTUSER != "" ]]; then
     brew=$(sudo -i -u $CURRENTUSER command -v brew)
+fi
 
-    if [[ $? = 1 ]]; then
-        brew="/usr/local/bin/brew"
+# If the Homebrew path was not found, try the default paths
+if [[ -z "$brew" ]]; then
+    arch_name="$(uname -m)"
+    if [ "${arch_name}" = "x86_64" ]; then
+        # Since munkireport uses an x86 version of Python 2.7 , it is possible we're on an
+        # Apple Silicon Mac but our current environment is x86 through Rosetta 2.
+        if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
+            brew="/opt/homebrew/bin/brew" # Running through Rosetta 2
+        else
+            brew="/usr/local/bin/brew" # Intel
+        fi 
+    elif [ "${arch_name}" = "arm64" ]; then
+        brew="/opt/homebrew/bin/brew"
     fi
-else
-    brew="/usr/local/bin/brew"
 fi
 
 
